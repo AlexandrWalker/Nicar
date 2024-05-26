5bb67c2d080289b554698d2a0e987d550db13967
AlexandrWalker <alexandr.d.walker@gmail.com>
2024-05-26 21:20:27 +0700

    done

--- script.js
+++ script.js
@@ -5,114 +5,119 @@
       {
         text: "Бабаевск",
         tel: "8(999)9999999",
+        address: "ул. Пушкина 5",
       },
       {
         text: "Архангельск",
         tel: "8(999)9999999",
+        address: "ул. Пушкина 5",
       },
       {
         text: "Новосибирск",
         tel: "8(999)9999999",
+        address: "ул. Пушкина 5",
       },
       {
         text: "Москва",
         tel: "8(999)9999999",
+        address: "ул. Пушкина 5",
       },
       {
         text: "Новгород",
         tel: "8(999)9999999",
+        address: "ул. Пушкина 5",
       },
       {
         text: "Нижний-Новгород",
         tel: "8(999)9999999",
+        address: "ул. Пушкина 5",
       },
       {
         text: "Владивосток",
         tel: "8(999)9999999",
+        address: "ул. Пушкина 5",
       },
       {
         text: "Воронеж",
         tel: "8(999)9999999",
+        address: "ул. Пушкина 5",
       },
       {
         text: "Зеленоград",
         tel: "8(999)9999999",
+        address: "ул. Пушкина 5",
       },
       {
         text: "Добрянка",
         tel: "8(999)9999999",
+        address: "ул. Пушкина 5",
       },
       {
         text: "Грозный",
         tel: "8(999)9999999",
+        address: "ул. Пушкина 5",
       },
       {
         text: "Красноярск",
         tel: "8(999)9999999",
+        address: "ул. Пушкина 5",
       },
       {
         text: "Краснодар",
         tel: "8(999)9999999",
+        address: "ул. Пушкина 5",
       },
       {
         text: "Пермь",
         tel: "8(999)9999999",
+        address: "ул. Пушкина 5",
       },
       {
         text: "Омск",
         tel: "8(999)9999999",
+        address: "ул. Пушкина 5",
       },
       {
         text: "Ярославль",
         tel: "8(999)9999999",
+        address: "ул. Пушкина 5",
       },
       {
         text: "Южно-Сахалинск",
         tel: "8(999)9999999",
+        address: "ул. Пушкина 5",
       },
       {
         text: "Нижний-Новгород",
         tel: "8(999)9999999",
+        address: "ул. Пушкина 5",
       },
       {
         text: "Владивосток",
         tel: "8(999)9999999",
+        address: "ул. Пушкина 5",
       },
       {
         text: "Воронеж",
         tel: "8(999)9999999",
+        address: "ул. Пушкина 5",
       },
       {
         text: "Зеленоград",
         tel: "8(999)9999999",
+        address: "ул. Пушкина 5",
       },
       {
         text: "Добрянка",
         tel: "8(999)9999999",
+        address: "ул. Пушкина 5",
       },
       {
         text: "Грозный",
         tel: "8(999)9999999",
+        address: "ул. Пушкина 5",
       },
     ];
-
-    const modal__window = document.querySelector(".modal__window");
-    const address = modal__window.querySelector(".address");
-    const addressItemValue = document.querySelectorAll('.address__item');
-    const modalLettersItem = document.querySelectorAll('.modal__letters-item');
-
-    const createAddressNodes = () => {
-      addressItem.forEach(({ text, tel, reaction }, i) => {
-        address.insertAdjacentHTML(
-          "beforeend",
-          `<li class="address__item" data-tel=${tel.replace(/[^0-9]/g, "")}">
-            ${text}
-          </li>`
-        );
-      });
-    };
-
-    createAddressNodes();
 
     const createLettersNodes = () => {
       function idsm(origin) {
@@ -123,9 +128,9 @@
       uniqueArr.forEach((uniqueArr, i) => {
         document.getElementById('myList').insertAdjacentHTML(
           "beforeend",
-          `<li class="modal__letters-item">
+          `<li><button class="modal__letters-item" href="#!" data-letter="${uniqueArr}">
             ${uniqueArr}
-          </li>`
+          </button></li>`
         );
       });
     };
@@ -176,29 +181,86 @@
 
       Array.from(lettersItem).forEach(function (item, i, lettersItem) {
         item.addEventListener('click', function (e) {
-          if (lettersActive.length > 0 && lettersActive[0] !== this)
+          if (lettersActive.length > 0 && lettersActive[0] !== this) {
             lettersActive[0].classList.remove('letters-active');
+          }
 
-          this.classList.toggle('letters-active');
+          let lettersСitiesItems = document.querySelectorAll('.letters__cities');
 
-          // Выбираем все div-элементы 
-          var els = document.querySelectorAll('.address__item');
+          for (let i = 0; i < lettersСitiesItems.length; i++) {
+            lettersСitiesItems[i].outerHTML = "";
+          }
 
-          var result = "";
+          this.classList.add('letters-active');
 
-          // Итерация по выбранным элементам
-          for (var i = 0; i < els.length; i++) {
+          var lettersFirstItem = this.getAttribute('data-letter');
 
-            // Получаем текстовое значение
-            var text = els[i].innerText;
+          function uniCities(origins) {
+            return origins.map(item => item.text);
+          }
+          const citiesArr = uniCities(addressItem);
+          const uniqueCities = [...new Set(citiesArr)];
 
-            // Проверяем, начинается ли текст с заданной литеры
-            if (text.startsWith(/^[\t\r\n]+$/.this)) {
-              result = result + text + " ";
-            }
+          let cityId = uniqueCities.filter(r => r[0] == lettersFirstItem)
+
+          const createCitiesList = () => {
+            addressItem.forEach(({ text, tel }, i) => {
+              if (typeof cityId[i] === "undefined") {
+                return
+              } else {
+                this.parentNode.insertAdjacentHTML(
+                  "beforeend",
+                  `<a class="letters__cities" href="#!" name=${cityId[i]} data-tel=${tel.replace(/[^0-9]/g, "")}>
+                    ${cityId[i]}
+                  </a>`
+                );
+              }
+            });
           };
-          console.log(result);
-          document.getElementsByClassName('result')[0].innerText = result;
+
+          createCitiesList();
+
+          var citiesItem = document.querySelectorAll('.letters__cities'),
+            citiesActive = document.getElementsByClassName('cities-active');
+
+          Array.from(citiesItem).forEach(function (item, i, citiesItem) {
+            item.addEventListener('click', function (e) {
+
+              if (citiesActive.length > 0 && citiesActive[0] !== this)
+                citiesActive[0].classList.remove('cities-active');
+
+              let addressCitiesItem = document.querySelectorAll('.address__item');
+
+              for (let i = 0; i < addressCitiesItem.length; i++) {
+                addressCitiesItem[i].outerHTML = "";
+              }
+
+              this.classList.add('cities-active');
+
+              let cityName = addressItem.filter(n => n.text == this.getAttribute('name'))
+
+              const address = document.getElementById("address");
+              const createAddressNodes = () => {
+                addressItem.forEach(({ text, tel }, i) => {
+                  if (typeof cityName[i] === "undefined") {
+                    return
+                  } else {
+                    address.insertAdjacentHTML(
+                      "beforeend",
+                      `<li class="address__item">
+                        <p class="address__item-city">${cityName[i].text}</p>
+                        <p class="address__item-address">${cityName[i].address}</p>
+                        <a class="address__item-link" href="tel:${cityName[i].tel.replace(/[^0-9]/g, "")}">${cityName[i].tel}</a>
+                      </li>`
+                    );
+                  }
+                });
+              };
+              createAddressNodes();
+
+            });
+          });
+
         });
       });
     })();
